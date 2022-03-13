1fa5c78827e666138d24451ac586d7bdaede5cbc
CrustaShrimp <60628153+CrustaShrimp@users.noreply.github.com>
2022-03-13 09:06:15 +0100

    Update ShuntingYard.h

--- ParseMath/ShuntingYard.h
+++ ParseMath/ShuntingYard.h
@@ -5,18 +5,52 @@
 
 std::deque<IMathToken*> ShuntingYard(const CString& strEquation)
 {
+    // Top
+    // Push
+    // Pop
+    std::stack<double>              NumStack;
+    std::stack<CMathTokenOperator>  OperatorStack;
+    
     // Step 1: Remove spaces
     strEquation.Remove(' ');
     // Step 2: Tokenize
-    const CString Seperator = _T(" ");
-    int Position = 0;
-    CString Token;
+    const CString strSeperators = _T("+-*/()^");
+    int iPosition = 0;
+    CString strToken;
 
-    Token = strEquation.Tokenize(Seperator, Position);
-    while (!Token.IsEmpty())
+    strToken = strEquation.Tokenize(strSeperator, iPosition);
+    while (!strToken.IsEmpty())
     {
+        // Check the seperator = operator
+        char cOperator = strSeperator[iPosition-1];
+        switch(cOperator)
+        {
+            case '^':
+                // Process power
+                break;
+            case ')':
+                break;
+            case '(':
+                break;
+            case '*':
+                break;
+            case '/':
+                break;
+            case '+':
+                break;
+            case '-':
+                break;
+            default:
+                assert(false);
+                break;
+        }
+        
+        // Process the token = value
+        const double dValue = std::stod(strToken);
+        NumStack.push(dValue);
+        
         // Get next token.
-        Token = strEquation.Tokenize(Seperator, Position);
+        strToken = strEquation.Tokenize(strSeperator, iPosition);
 
         // Process token into IMathToken
     }
